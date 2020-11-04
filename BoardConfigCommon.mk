fe906d36a3a75f1d751799505c09800f4a9ad9c6
rlawoehd187 <rlawoehd187@naver.com>
2020-11-05 04:58:32 +0900

    temporary sepolicy for extra root dirs

--- BoardConfigCommon.mk
+++ BoardConfigCommon.mk
@@ -160,10 +160,16 @@ BOARD_USES_QCOM_HARDWARE := true
 TARGET_RECOVERY_FSTAB := $(COMMON_PATH)/rootdir/etc/fstab.qcom
 
 # SELinux
-include device/qcom/sepolicy-legacy/sepolicy.mk
+#include device/qcom/sepolicy-legacy/sepolicy.mk
+
+#BOARD_SEPOLICY_DIRS += \
+#    $(COMMON_PATH)/sepolicy
 
 BOARD_SEPOLICY_DIRS += \
-    $(COMMON_PATH)/sepolicy
+    $(COMMON_PATH)/sepolicy-tmp
+
+
+SELINUX_IGNORE_NEVERALLOWS := true
 
 # Sensor Compat
 BOARD_GLOBAL_CFLAGS += -DCOMPAT_SENSORS_M
