f3753b40a6f846729a0a33a67c03f3d641d40e4b
hiru <rlawoehd187@naver.com>
2021-03-14 22:57:11 +0900

    msm8974-common: sepolicy enforcing test

--- BoardConfigCommon.mk
+++ BoardConfigCommon.mk
@@ -167,14 +167,9 @@ BOARD_USES_QCOM_HARDWARE := true
 TARGET_RECOVERY_FSTAB := $(COMMON_PATH)/rootdir/etc/fstab.qcom
 
 # SELinux
-#include device/qcom/sepolicy-legacy/sepolicy.mk
+include device/qcom/sepolicy-legacy/sepolicy.mk
 
-#BOARD_SEPOLICY_DIRS += \
-#    $(COMMON_PATH)/sepolicy
-
-BOARD_SEPOLICY_DIRS += \
-    $(COMMON_PATH)/sepolicy-tmp
-
+BOARD_SEPOLICY_DIRS += $(PLATFORM_PATH)/sepolicy
 
 SELINUX_IGNORE_NEVERALLOWS := true
 
